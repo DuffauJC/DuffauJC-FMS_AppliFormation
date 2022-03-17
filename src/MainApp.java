9e9043a5fcb7dd798675c243e571f34da9a06c3a
DuffauJC <jeancharles.duffau@gmail.com>
2022-03-17 10:57:57 +0100

    Modification, correction du bug panier, ajout de la quantité au panier, modification javadoc

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,8 +7,10 @@ import java.util.Scanner;
 
 public class MainApp {
 
-	//  hashmap des formations
+	//  hashmap liste présentation des formations
 	protected static Map<Integer,ArrayList<String>> formationList = new HashMap<>();
+	//  hashmap du choix des formations
+	protected static Map<Integer,ArrayList<String>> formationChoix = new HashMap<>();
 	//  hashmap du panier
 	protected static Map<Integer,ArrayList<String>> caddyList = new HashMap<>();
 
@@ -20,6 +22,7 @@ public class MainApp {
 	public static void main(String[] args) {
 
 		insertFormationList();
+		formationChoix();
 		mainFunction();
 		scan.close();
 	}
@@ -75,66 +78,60 @@ public class MainApp {
 
 	/** Méthode qui recherche une formation dans la liste contenant un mot clé saisi au clavier */
 	public static void findFormation() {
-		displayFormations();
-		
-		System.out.println("Entrée le nom du cours ex : Spring");
-		
-		String keyword=scan.nextLine();
-		
-		System.out.println("keyword "+keyword);
+		displayFormationsChoix();
 
-		// comparaison de chaine  java/java avance
+		System.out.println("Entrée le choix du cours ex : 1");
+		String keyword=scan.next();
 
-		for (Integer i : formationList.keySet()) {
+		System.out.println("Entrée la quantité ex : 6");
+		String qty=scan.next();
 
-			for (String str : formationList.get(i)) {
+		formationChoix.forEach((key,value)->{	
+			if(value.get(0).contains(keyword)) { 
+				// ajout de la quantité dans l'arraylist avant ajout au caddy
+				value.add(qty);
+				addFormationCaddy(value);
+			}					
+		});
 
-				if (str.contentEquals(keyword)) {
-					System.out.println(str);
-					//addFormationCaddy(formationList.get(i));
-				}
-
-			}
-		}
 	}
 
 	/** Méthode qui ajoute une formation au panier
 	 * @param value  */
 	public static void addFormationCaddy(ArrayList<String> value) {
 
+		
 		int key=1;
-		System.out.println("valeur à ajouter"+value);
+		int size;
+		// le caddy est-il vide ?
+		if (caddyList.isEmpty()){
+			caddyList.put(key,value);
+		}
 
-		/*
-		 * // le caddy est-il vide ? si oui clé initialisé à 1 if (caddyList.size()==0)
-		 * { caddyList.put(key, value);
-		 * 
-		 * // sinon on récupère la taille du hashmap + incrémentation }else { key=0;
-		 * key=caddyList.size()+1; caddyList.put(key, value);
-		 * System.out.println("size :"+caddyList.size()); }
-		 */
+		// sinon on récupère la taille du hashmap + incrémentation } 
+		else { 
+			size=caddyList.size();
+			key=size+1; 
+			caddyList.put(key, value);
 
+		}
 	}
 
 	/** Méthode qui affiche le panier  */
 	public static void displayCaddy() {
-
+		
 		System.out.println("Caddy.\n");
-		System.out.println("---------------------------------------------------------------------------------------");
-		System.out.printf("| %-5s| %-15s | %-5s | %-40s |%-5s |%n", "No", "COURS", "NB/JOURS", "DESCRIPTION"," PRIX");
-		System.out.println("|------|-----------------|----------|------------------------------------------|------|");
+		System.out.println("--------------------------------------------------------------------------------------------");
+		System.out.printf("| %-5s| %-15s | %-5s | %-40s |%-5s | %-2s |%n", "CHOIX", "COURS", "NB/JOURS", "DESCRIPTION"," PRIX","QTE");
+		System.out.println("|------|-----------------|----------|------------------------------------------|------|-----|");
 
-
-		caddyList.forEach((key,value)->{	
 			for (int i = 1; i < caddyList.size()+1; i++) {
-				System.out.printf("| %-5s| %-15s | %-8s | %-40s | %-3s |%n",key, caddyList.get(i).get(0), 
+				System.out.printf("| %-5s| %-15s | %-8s | %-40s | %-3s |  %-2s |%n",caddyList.get(i).get(0), 
 						caddyList.get(i).get(1),
-						caddyList.get(i).get(2),caddyList.get(i).get(3));
-
+						caddyList.get(i).get(2),caddyList.get(i).get(3),caddyList.get(i).get(4),caddyList.get(i).get(5));
 			}			
-		});
-
-		System.out.println("---------------------------------------------------------------------------------------");
+		
+		System.out.println("---------------------------------------------------------------------------------------------");
 
 	}
 
@@ -149,9 +146,25 @@ public class MainApp {
 		for (int i = 1; i < formationList.size()+1; i++) {
 			System.out.printf("| %-15s | %-8s | %-40s | %-3s |%n", formationList.get(i).get(0), formationList.get(i).get(1),
 					formationList.get(i).get(2),formationList.get(i).get(3));
-
 		}
 		System.out.println("--------------------------------------------------------------------------------");
+
+	}
+
+	/** Méthode qui affiche la liste de toutes les formations pour l'ajout au panier  */
+	public static void displayFormationsChoix() {
+
+		System.out.println("Formations.\n");
+		System.out.println("---------------------------------------------------------------------------------------");
+		System.out.printf("| %-5s | %-15s | %-5s | %-40s |%-5s |%n", "CHOIX", "COURS", "NB/JOURS", "DESCRIPTION"," PRIX");
+		System.out.println("|-------|-----------------|----------|------------------------------------------|------|");
+
+		for (int i = 1; i < formationChoix.size()+1; i++) {
+			System.out.printf("| %-5s | %-15s | %-8s | %-40s | %-3s |%n", formationChoix.get(i).get(0), formationChoix.get(i).get(1),
+					formationChoix.get(i).get(2),formationChoix.get(i).get(3),formationChoix.get(i).get(4));
+
+		}
+		System.out.println("----------------------------------------------------------------------------------------");
 
 	}
 
@@ -186,6 +199,44 @@ public class MainApp {
 		formationList.get(5).add("20");		
 		formationList.get(5).add("DontNet Core");
 		formationList.get(5).add("5000");
+	}
+
+	/** Méthode qui initialise la liste des formations */
+	private static void formationChoix() {
+		formationChoix.put(1, new ArrayList<String>());
+		formationChoix.get(1).add("1");
+		formationChoix.get(1).add("Java"); 
+		formationChoix.get(1).add("20");		
+		formationChoix.get(1).add("Java SE 8 : Syntaxe et POO");
+		formationChoix.get(1).add("3000");
+
+		formationChoix.put(2, new ArrayList<String>());
+		formationChoix.get(2).add("2"); 
+		formationChoix.get(2).add("Java avancé"); 	
+		formationChoix.get(2).add("20");	
+		formationChoix.get(2).add("Exceptions, fichiers, JDBC, thread");
+		formationChoix.get(2).add("5000");
+
+		formationChoix.put(3, new ArrayList<String>());
+		formationChoix.get(3).add("3"); 
+		formationChoix.get(3).add("Spring"); 	
+		formationChoix.get(3).add("20");		
+		formationChoix.get(3).add("Spring Core/MVC/Security");
+		formationChoix.get(3).add("5000");
+
+		formationChoix.put(4, new ArrayList<String>());
+		formationChoix.get(4).add("4"); 
+		formationChoix.get(4).add("Php frameworks"); 	
+		formationChoix.get(4).add("15");		
+		formationChoix.get(4).add("Symphony");
+		formationChoix.get(4).add("2500");
+
+		formationChoix.put(5, new ArrayList<String>());
+		formationChoix.get(5).add("5"); 
+		formationChoix.get(5).add("C#"); 	
+		formationChoix.get(5).add("20");		
+		formationChoix.get(5).add("DontNet Core");
+		formationChoix.get(5).add("5000");
 	}
 
 }
