af0d9bcf768ba32b35cbdbdbdd9be23efc92930f
DuffauJC <jeancharles.duffau@gmail.com>
2022-03-18 14:29:15 +0100

    Ajout de la validation commande, de la javadoc, plus un jeu bonus

--- src/MainApp.java
+++ src/MainApp.java
@@ -4,13 +4,27 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-
+/**
+ * 
+ * Application permettant de commander des formations.
+ * Lister, Ajouter au panier, Modifier panier, Valider la commande.
+ * Petit jeu en bonus.
+ * 
+ * @author Jean-Charles Duffau
+ * @version 4.0
+ * 
+ * 
+ * */
 public class MainApp {
 
 	//  hashmap liste présentation des formations
 	protected static Map<Integer,ArrayList<String>> formationList = new HashMap<>();
 	//  hashmap du choix des formations
 	protected static Map<Integer,ArrayList<String>> formationChoix = new HashMap<>();
+
+	//  hashmap  formations futures
+	protected static Map<Integer,ArrayList<String>> futur= new HashMap<>();
+
 	//  hashmap du panier
 	protected static Map<Integer,ArrayList<String>> caddyList = new HashMap<>();
 
@@ -18,11 +32,16 @@ public class MainApp {
 
 
 
-
+/**
+ * Main principal
+ * 
+ * @param args execution des fonctions principales
+ */
 	public static void main(String[] args) {
 
 		insertFormationList();
 		formationChoix();
+		insertFormationFutur();
 		mainFunction();
 		scan.close();
 	}
@@ -38,7 +57,7 @@ public class MainApp {
 
 		int ans=0;
 
-		while(ans != 4) {
+		while(ans != 7) {
 			// Affichage du menu
 			showMenu();
 
@@ -58,11 +77,23 @@ public class MainApp {
 				findFormation();
 				break;
 
-			case 3 : 	// Afficher panier
+			case 3 : 	// Afficher et modifier le panier
 				displayCaddy();
 				break;
 
-			case 4 : // Exit application
+			case 4 : 	// Valider la commande
+				displayCommand();
+				break;
+
+			case 5 : 	// Prochainement disponible
+				inTheFutur();
+				break;
+
+			case 6 : 	//Envie de jouer
+				game();
+				break;
+				
+			case 7 : // Exit application
 				System.out.println("Good Bye.");
 				break;
 
@@ -72,13 +103,17 @@ public class MainApp {
 
 
 	}
+
 	/** Méthode qui affiche le menu  */
 	public static void showMenu() {
 		System.out.println("Menu :\n");
 		System.out.println("1.Voulez-vous voir la formations ?");
 		System.out.println("2.Ajouter une formation au panier en fonction du cours.");
-		System.out.println("3.Afficher le panier.");
-		System.out.println("4.Sortir de l'application.\n");
+		System.out.println("3.Afficher/Modifier le panier.");
+		System.out.println("4.Valider la commande.");
+		System.out.println("5.Prochainement....");
+		System.out.println("6.Envie de jouer....");
+		System.out.println("7.Sortir de l'application.\n");
 	}
 
 	/** Méthode qui recherche une formation dans la liste contenant un mot clé saisi au clavier */
@@ -115,7 +150,9 @@ public class MainApp {
 	}
 
 	/** Méthode qui ajoute une formation au panier
-	 * @param value  */
+	*
+	 * @param value , valeur à ajouter au panier
+	 */
 	public static void addFormationCaddy(ArrayList<String> value) {
 
 		int key=1;
@@ -136,32 +173,8 @@ public class MainApp {
 	/** Méthode qui affiche le panier  */
 	public static void displayCaddy() {
 
-		System.out.println("Caddy\n");
-		if (caddyList.isEmpty()) {
-			System.out.println("---------------------");
-			System.out.println("Votre panier est vide");
-			System.out.println("---------------------");
-		} else {
+		caddy();
 
-			System.out.println("--------------------------------------------------------------------------------------------");
-<<<<<<< HEAD
-			System.out.printf("| %-5s| %-15s | %-5s | %-40s |%-5s | %-2s |%n", "CHOIX", "COURS", "NB/JOURS", "DESCRIPTION"," PRIX","QTE");
-=======
-			System.out.printf("| %-5s| %-15s | %-5s | %-40s |%-5s | %-2s |%n", "NO.", "COURS", "NB/JOURS", "DESCRIPTION"," PRIX","QTE");
->>>>>>> delCaddy
-			System.out.println("|------|-----------------|----------|------------------------------------------|------|-----|");
-
-			for (int i = 1; i < caddyList.size()+1; i++) {
-				System.out.printf("| %-5s| %-15s | %-8s | %-40s | %-3s |  %-2s |%n",caddyList.get(i).get(0), 
-						caddyList.get(i).get(1),
-						caddyList.get(i).get(2),caddyList.get(i).get(3),caddyList.get(i).get(4),caddyList.get(i).get(5));
-			}			
-
-			System.out.println("---------------------------------------------------------------------------------------------");
-		}
-
-<<<<<<< HEAD
-=======
 		int rep=1;
 
 		while(rep != 3) {
@@ -189,8 +202,9 @@ public class MainApp {
 						System.out.println("Entrée la nouvelle quantité.");
 						String qty=scan.next();
 						value.set(5,qty);
+						caddy();
+					}
 
-					}					
 				});
 				break;
 
@@ -202,6 +216,7 @@ public class MainApp {
 				caddyList.entrySet()
 				.removeIf(key -> key.getValue().contains(keyword));
 
+				caddy();
 				break;
 
 			case  3: // Exit application
@@ -212,9 +227,79 @@ public class MainApp {
 			}	
 		}
 
->>>>>>> delCaddy
+	}
+	/** Méthode qui affiche le  caddy  */
+	private static void caddy() {
+
+		if (caddyList.isEmpty()) {
+			System.out.println("---------------------");
+			System.out.println("Votre panier est vide");
+			System.out.println("---------------------");
+		} else {
+			System.out.println("Caddy\n");
+			System.out.println("--------------------------------------------------------------------------------------------");
+			System.out.printf("| %-5s| %-15s | %-5s | %-40s |%-5s | %-2s |%n", "NO.", "COURS", "NB/JOURS", "DESCRIPTION"," PRIX","QTE");
+			System.out.println("|------|-----------------|----------|------------------------------------------|------|-----|");
+
+			for (int i = 1; i < caddyList.size()+1; i++) {
+				System.out.printf("| %-5s| %-15s | %-8s | %-40s | %-3s |  %-2s |%n",caddyList.get(i).get(0), 
+						caddyList.get(i).get(1),
+						caddyList.get(i).get(2),caddyList.get(i).get(3),caddyList.get(i).get(4),caddyList.get(i).get(5));
+			}			
+
+			System.out.println("---------------------------------------------------------------------------------------------");
+		}
 
 	}
+
+	/** Méthode qui affiche la commande  */
+	public static void displayCommand() {
+
+		if (caddyList.isEmpty()) {
+			System.out.println("-----------------------------");
+			System.out.println("Vous n'avez pas de commande.");
+			System.out.println("-----------------------------");
+		} else {
+			System.out.println("Votre commande\n");
+			System.out.println("--------------------------------------------------------------------------------------------");
+			System.out.printf("| %-5s| %-15s | %-5s | %-40s |%-5s | %-2s |%n", "NO.", "COURS", "NB/JOURS", "DESCRIPTION"," PRIX","QTE");
+			System.out.println("|------|-----------------|----------|------------------------------------------|------|-----|");
+
+			for (int i = 1; i < caddyList.size()+1; i++) {
+				System.out.printf("| %-5s| %-15s | %-8s | %-40s | %-3s |  %-2s |%n",caddyList.get(i).get(0), 
+						caddyList.get(i).get(1),
+						caddyList.get(i).get(2),caddyList.get(i).get(3),caddyList.get(i).get(4),caddyList.get(i).get(5));
+			}			
+
+			System.out.println("---------------------------------------------------------------------------------------------");
+			// calcul du montant de la commande
+			int total=0;
+			for (int i = 1; i < caddyList.size()+1; i++) {
+				int prix =Integer.parseInt(caddyList.get(i).get(4));
+				int qty =Integer.parseInt(caddyList.get(i).get(5));
+
+				int subTotal=prix*qty;
+				total+=subTotal;
+			}
+			// affichage du total de la commande
+			System.out.printf("| %81s | %-2s |%n"," TOTAL COMMANDE",total);
+			System.out.println("---------------------------------------------------------------------------------------------");		
+
+			// si commande on peut valider
+			System.out.println("Valider la commande Y/N ?");
+			String rep = scan.next().toUpperCase();
+
+			if (rep.equals("Y")) {
+				caddyList.clear();
+				System.out.println("Commande validée.");
+			} else {
+				System.out.println("Panier toujours valide");
+
+			}
+		}
+
+	}
+
 
 	/** Méthode qui affiche la liste de toutes les formations  */
 	public static void displayFormations() {
@@ -246,6 +331,90 @@ public class MainApp {
 
 		}
 		System.out.println("----------------------------------------------------------------------------------------");
+
+	}
+
+	private static void inTheFutur() {
+		System.out.println("Formations futures.\n");
+		System.out.println("------------------------------------------------------------------------");
+		System.out.printf("| %-15s | %-5s | %-40s |%n", "COURS", "NB/JOURS", "DESCRIPTION");
+		System.out.println("|-----------------|----------|------------------------------------------|");
+
+		for (int i = 1; i < futur.size()+1; i++) {
+			System.out.printf("| %-15s | %-8s | %-40s |%n", futur.get(i).get(0), futur.get(i).get(1),
+					futur.get(i).get(2));
+		}
+		System.out.println("-------------------------------------------------------------------------");
+
+	}
+	
+	/** Méthode qui permet de jouer */
+	private static void game() {
+		// initializing variables
+				boolean saisie;
+				int nbr=0;
+				int count=0;
+				int rep;
+				int one=1;
+				int random = 0;
+
+				System.out.println("Veux-tu jouer à mon jeux ? Tapes 1 pour jouer");
+				saisie = scan.hasNextInt();
+
+				// verif si c'est un chiffre de taper
+				if (saisie) {
+					rep = scan.nextInt();
+
+					if (rep == one) {
+
+						// random
+						random=(int)(Math.random()*100)+1 ;
+
+						// choose number between 1 and 100
+						System.out.println("Saisir un nombre entre 1 et 100");
+						nbr=scan.nextInt();
+
+						while (nbr != random) {
+
+							count++; 
+							if (nbr<random) {
+								System.out.println("Saisir une valeur plus grande"); 
+								nbr=scan.nextInt();
+
+							} else if(nbr>random) { System.out.println("Saisir une valeur plus petite");
+							nbr=scan.nextInt();
+							} 
+						} 
+						if(nbr==random) {
+							System.out.println("Vous avez trouvé en "+ count +" coups."); 
+						}
+					}else {
+						System.out.println("Good Bye");
+					}
+
+				} else {
+					System.out.println("La valeur rentrée n'était pas du type voulu");
+				}
+	}
+	
+	/** Méthode qui initialise la liste des formations */
+	private static void insertFormationFutur() {
+		futur.put(1, new ArrayList<String>());
+		futur.get(1).add("Yoga"); 	
+		futur.get(1).add("20");		
+		futur.get(1).add("Le yoga pour les nuls.");
+		
+
+		futur.put(2, new ArrayList<String>());
+		futur.get(2).add("Poney"); 	
+		futur.get(2).add("45");	
+		futur.get(2).add("Le poney pour les nuls");
+	
+
+		futur.put(3, new ArrayList<String>());
+		futur.get(3).add("Sièste"); 	
+		futur.get(3).add("90");		
+		futur.get(3).add("La sièste pour les nuls");
 
 	}
 
